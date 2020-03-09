f7d0d97cfdb41b9b6ec223d322959b801549c3ac
Justin Rokisky <jrokisky831@gmail.com>
2020-03-09 18:04:06 -0400

    Added pollards rho and switched trial division to finding a single factor

--- main.py
+++ main.py
@@ -1,56 +1,76 @@
 import random as r
 import argparse
 import timeit as t
+import math as m
 
+# Good test numbers:
+#    -> 636086005275917759
 
 def main():
+
+    # Parse user arguments.
     parser = argparse.ArgumentParser()
-    parser.add_argument('--digits', type=int, default=24, help='Number of digits in integer to factor')
+    parser.add_argument('--number', type=int, default=12345, help='Number to find a factor of.')
+    parser.add_argument('--digits', type=int, help='Number of digits of randomly generated integer to factor')
     parser.add_argument('--method', type=str, default='both', help='Factoring Method: both, trial_division or pollards_rho')
     args = parser.parse_args()
 
+    # Ensure a valid factoring method was provided
     if args.method not in ['trial_division', 'pollards_rho', 'both']:
         parser.print_help()
         raise SystemExit()
 
-    num = gen_rand_int(args.digits)
+    # Factor a randomly generated number or a given number.
+    print(args)
+    if args.digits is not None:
+        num = gen_rand_int(args.digits)
+    else:
+        num = args.number
+
     print("Number to factor:")
     print(str(num))
 
+    # Find a factor via trial division.
     if args.method == 'trial_division' or args.method == 'both':
         start_td = t.default_timer()
-        td_factors = trial_division(num)
+        td_factor = trial_division(num)
         end_td = t.default_timer()
         run_td = end_td - start_td
-        print("TD | Factors: " + td_factors + " Run: " + str(run_td)) 
+        print("TD | Factor: " + str(td_factor) + " Run: " + str(run_td)) 
 
+    # Find a factor via pollard's rho.
     if args.method == 'pollards_rho' or args.method == 'both':
         start_pr = t.default_timer()
-        pr_factors = pollards_rho(num)
+        pr_factor = pollards_rho(num)
         end_pr = t.default_timer()
         run_pr = end_pr - start_pr
-        print("PR | Factors: " + pr_factors + " Run: " + str(run_pr)) 
+        print("PR | Factor: " + str(pr_factor) + " Run: " + str(run_pr)) 
 
-
+# Find a factor via trial division.
 def trial_division(num):
-    n = 1
-    small_factors = []
-    large_factors = []
+    n = 2
     while n**2 < num:
         if num % n == 0:
-            small_factors.append(n)
-            large_factors.insert(0, num // n)
+            return n
         n += 1
-    factors = small_factors + large_factors
+    return num
 
-    return ','.join(str(f) for f in factors)
-
-
+# Find a factor via pollard's rho.
 def pollards_rho(num):
-    return ""
+    x = 2
+    y = 2
+    d = 1
+
+    fn = lambda x: (x**2 + 1) % num
+    while d == 1:
+        x = fn(x)
+        y = fn(fn(y))
+        d = m.gcd(abs(x-y), num)
+
+    return d
 
 
-
+# Generate a random integer of a certain number of digits.
 def gen_rand_int(num_digits):
     rand = 0
     for n in range(0, num_digits):
