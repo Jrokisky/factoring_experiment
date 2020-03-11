d53a5100a950f73bbb5427b5cfbac696f4adeb6b
576765 <andrenguyen@C02YX119LVDT.local>
2020-03-11 17:10:20 -0400

    refactor order of operations when generating results

--- generate_results.py
+++ generate_results.py
@@ -6,13 +6,17 @@ nums = gen_semiprimes(return_factors=True)
 
 with open('./results_trial_division.txt','w') as f:
 	f.write(','.join(['n','p1','p2','pr_factor','run_pr','peak'])+'\n')
-	for n in tqdm(nums):
-		res = find_factor(n[0], 'trial_division', verbose=False)
-		f.write(','.join([str(n[0]),str(n[1]),str(n[2]),str(res[0]),str(res[1]),str(res[2])])+'\n')
 
 with open('./results_pollards_rho.txt','w') as f:
 	f.write(','.join(['n','p1','p2','pr_factor','run_pr','peak'])+'\n')
-	for n in tqdm(nums):
+
+for n in tqdm(nums):
+
+	with open('./results_trial_division.txt','w') as f:
+		res = find_factor(n[0], 'trial_division', verbose=False)
+		f.write(','.join([str(n[0]),str(n[1]),str(n[2]),str(res[0]),str(res[1]),str(res[2])])+'\n')
+
+	with open('./results_pollards_rho.txt','w') as f:
 		res = find_factor(n[0], 'pollards_rho', verbose=False)
 		f.write(','.join([str(n[0]),str(n[1]),str(n[2]),str(res[0]),str(res[1]),str(res[2])])+'\n')
 
