b416733336d3d7cea9b67c55b2452bbdba137e6d
576765 <andrenguyen@C02YX119LVDT.local>
2020-03-11 23:09:11 -0400

    commit to pull

--- analysis.py
+++ analysis.py
@@ -1,2 +1,10 @@
+import pandas as pd
+
+df_td = pd.read_csv('./results_trial_division.txt')
+print(df_td.head())
+
+df_pr = pd.read_csv('./results_pollards_rho.txt')
+print(df_pr.head())
+
 # https://stackoverflow.com/questions/22481854/plot-mean-and-standard-deviation
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
